b735f76dfca88a8af3b728e7cb6748eacb7fb044
chaewon <cwlee4633@naver.com>
2023-06-13 15:19:15 +0900

    구구단 입력한 단 제외하고 출력하기 기능 추가

--- src/week02/base/W10.java
+++ src/week02/base/W10.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class W10 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int passNum = sc.nextInt();
+
         for (int i = 2; i <= 9 ; i++) {
+            if(i == passNum){
+                continue;
+            }
             for (int j = 1; j <= 9; j++){
                 System.out.println(i + " X " + j + " = " + (i*j));
             }
@@ -12,8 +19,3 @@ public class W10 {
     }
 }
 
-// for 문
-
-// for 문
-
-// 향상된 for 문
